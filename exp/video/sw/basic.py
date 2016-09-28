bf8935de26d3c1d31a4167b522c100acb9a81625
Pierre Surply <surply@lse.epita.fr>
2016-09-28 11:29:27 +0200

    VIDEO: Add basic display logic

---
+++ exp/video/sw/basic.py
@@ -0,0 +1,67 @@
+from bmii import *
+
+from video import *
+
+
+class BasicVideoIOModule(VideoIOModule):
+    sprite_addr = 0x0
+
+    @property
+    def display_logic(self):
+        bg = Signal(3)
+        self.comb += bg.eq(self.h_counter[6:9])
+
+        fg = Signal(3)
+        self.comb += fg.eq(self.vram_data)
+
+        return [self.px.eq(bg ^ fg)]
+
+    @property
+    def vram_read_logic(self):
+        return [
+            NextValue(self.vram_addr, self.sprite_addr +
+                self.h_counter[0:5] + (self.v_counter[1:5] << 5))
+        ]
+
+    def __init__(self):
+        VideoIOModule.__init__(self, VideoConfig.CFG2)
+
+
+class BasicVideo(Video):
+    sprite = [
+        0b00000000000000000000000000,
+        0b00000000000000000000000000,
+        0b11100000011111100011111111,
+        0b11100000111111110011111111,
+        0b11100001110000111011100000,
+        0b11100001100000011011100000,
+        0b11100001110000000011100000,
+        0b11100000111111100011111100,
+        0b11100000001111110011111100,
+        0b11100000000000111011100000,
+        0b11100001100000011011100000,
+        0b11100001110000111011100000,
+        0b11111110111111110011111111,
+        0b11111110011111100011111111,
+        0b00000000000000000000000000,
+        0b00000000000000000000000000,
+    ]
+
+    def __init__(self, config):
+        BMIIModule.__init__(self, BasicVideoIOModule())
+
+    def write_sprite(self, color):
+        d = []
+        for l in self.sprite:
+            for i in range(31, -1, -1):
+                d.append(color if (l >> i & 1) else 0)
+        self.write_vram(self.iomodule.sprite_addr, *d)
+
+
+if __name__ == "__main__":
+    b = BMII.default()
+    v = BasicVideo.default(b)
+
+    b.cli()
+    b.usbctl.drv.set_cpu_speed(CPUSpd.CLK_48M)
+    v.write_sprite(0b111)
